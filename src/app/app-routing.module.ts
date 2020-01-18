ba01669f7ec8065871424163d6959e5c3e7fc814
Abraham Zakharov <abrahamzakharov@gmail.com>
2020-01-18 17:35:22 -0500

    main page, sign up, and sign in designs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+      path: '',
+      component: MainComponent
+    },
+    {
+    path: 'login',
+    component: LoginComponent
+  }
+  ,
+    {
+    path: 'signup',
+    component: SignupComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
